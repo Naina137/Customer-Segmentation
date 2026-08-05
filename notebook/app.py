762dd363ddbc6bf04383651e0a8fc6d44b6b8fad
Naina137 <nainaguptaias407@gmail.com>
2026-08-05 19:05:36 +0530

    Added dashboard

--- notebook/app.py
+++ notebook/app.py
@@ -1,3 +1,4 @@
+
 import streamlit as st
 
 st.set_page_config(page_title="Customer Segmentation", layout="wide")
@@ -9,7 +10,7 @@ st.write("Welcome to the Customer Segmentation Project.")
 st.success("Deployment Successful!")
 
 st.markdown("""
-# Project Overview
+### Project Overview
 This project uses the K-Means clustering algorithm to segment customers based on:
 - Age
 - Annual Income
@@ -17,3 +18,21 @@ This project uses the K-Means clustering algorithm to segment customers based on
 
 The notebook contains preprocessing, clustering, visualization, and evaluation.
 """)
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+st.header("Dataset Preview")
+
+df = pd.read_csv("data/Mall_Customers.csv")
+
+st.dataframe(df.head())
+
+st.subheader("Dataset Information")
+st.write(df.shape)
+
+st.subheader("Annual Income Distribution")
+
+fig, ax = plt.subplots()
+ax.hist(df["Annual Income (k$)"], bins=10)
+st.pyplot(fig)
